8c1471bbf04174663986cf16583de016b9d57ca4
Lucas Oliveira <lucas.oliveira92@outlook.com>
2016-09-18 22:47:15 -0300

    removido alguns links, decidi fazer apenas um cardapio online

--- WebApp/App/admin/routes.js
+++ WebApp/App/admin/routes.js
@@ -2,8 +2,6 @@
     .module("app", ["ngRoute"])
     .config(function ($routeProvider) {
         $routeProvider
-            .when("/orders", { templateUrl: "/App/admin/views/orders/index.html" })
-            .when("/clients", { templateUrl: "/App/admin/views/clients/index.html" })
             .when("/pizzas", { templateUrl: "/App/admin/views/pizzas/index.html" })
             .when("/pizzas/novo", { templateUrl: "/App/admin/views/pizzas/form.html" })
             .when("/pizzas/editar/:id", { templateUrl: "/App/admin/views/pizzas/form.html" });
